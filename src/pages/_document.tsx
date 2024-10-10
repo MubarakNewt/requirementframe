42545eb02ef4e52660c091518bcbc4edad4c7b68
Mubarak <mubaraknewt10@gmail.com>
2024-10-10 15:59:18 +0100

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { config } from "../config/config";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta property="og:title" content={`Thirdweb Frame`} />
+        <meta property="og:image" content={`${config.hostUrl}/first.png`} />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content={`${config.hostUrl}/first.png`} />
+        <meta property="fc:frame:post_url" content={`${config.hostUrl}/api/mint`} />
+        <meta property="fc:frame:button:1" content="Get started" />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ src/pages/mint.tsx
@@ -0,0 +1,12 @@
+export default function FrameMint() {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+        }}>
+            <h1>Mint</h1>
+        </div>
+    )
+}
